7b92c246469d07784712bd7a382a98757da8ad4c
tyral <tyral@zklink.org>
2022-05-16 17:48:46 +0800

    refactor: order test

--- tests/matching.test.ts
+++ tests/matching.test.ts
@@ -12,86 +12,8 @@ import { getTestWallet, getWalletFromPrivateKey } from './wallet.test'
 const defaultValidFrom = 0
 const defaultValidUntil = 9007199254740991
 
-describe('Order', () => {
-  // it('sign sync order matching 1', async function () {
-  //   const wallet = await getWalletFromPrivateKey()
-
-  //   const data = {
-  //     fee: '0',
-  //     type: 'OrderMatching',
-  //     maker: {
-  //       nonce: 1,
-  //       price: '1500000000000000000',
-  //       amount: '100000000000000000000',
-  //       isSell: 0,
-  //       slotId: 1,
-  //       feeRatio: 10,
-  //       accountId: 6,
-  //       signature: {
-  //         pubKey: '0dd4f603531bd78bbecd005d9e7cc62a794dcfadceffe03e269fbb6b72e9c724',
-  //         signature:
-  //           '36525971db902fb7f8af094b24a835c9868b07599514670a93328da09357eb2e4dd3af60c89d401a682a9ec917ae8c4030a75e3dfc6f04aa9cecd17d13bb9101',
-  //       },
-  //       validFrom: 0,
-  //       validUntil: 9007199254740991,
-  //       baseTokenId: 6,
-  //       quoteTokenId: 7,
-  //       subAccountId: 1,
-  //     },
-  //     nonce: 0,
-  //     taker: {
-  //       nonce: 0,
-  //       price: '1500000000000000000',
-  //       amount: '1000000000000000000',
-  //       isSell: 1,
-  //       slotId: 3,
-  //       feeRatio: 10,
-  //       accountId: 6,
-  //       signature: {
-  //         pubKey: '0dd4f603531bd78bbecd005d9e7cc62a794dcfadceffe03e269fbb6b72e9c724',
-  //         signature:
-  //           '44ebf850ea8a3324f8ddc42e0ba9925678cf79e9909d3b9281724dc7b3076028d40aef64bfe6fc78b2d427f4b3ea8867de1f7774d40bccdcb4cce946472d0c03',
-  //       },
-  //       validFrom: 0,
-  //       validUntil: 9007199254740991,
-  //       baseTokenId: 6,
-  //       quoteTokenId: 7,
-  //       subAccountId: 1,
-  //     },
-  //     account: '0x3498F456645270eE003441df82C718b56c0e6666',
-  //     feeToken: 7,
-  //     accountId: 6,
-  //     signature: {
-  //       pubKey: '0dd4f603531bd78bbecd005d9e7cc62a794dcfadceffe03e269fbb6b72e9c724',
-  //       signature:
-  //         '46ab809c4a5beacfd423bb05332e818fb65d4d570939852206565c9a22adad8a3c3b4e69832c93f5b54b317bebcb44292bce5142f4bfa39f1b51a1cadcacae03',
-  //     },
-  //     expectBaseAmount: '1000000000000000000',
-  //     expectQuoteAmount: '1500000000000000000',
-  //   }
-  //   const maker = await wallet.signSyncOrder(data.maker as any)
-  //   const taker = await wallet.signSyncOrder(data.taker as any)
-
-  //   const signedTransaction = await wallet.signSyncOrderMatching({
-  //     type: 'OrderMatching',
-  //     accountId: 1,
-  //     account: '0x3498F456645270eE003441df82C718b56c0e6666',
-  //     fee: BigNumber.from('0'),
-  //     feeToken: 7,
-  //     nonce: 0,
-  //     expectBaseAmount: BigNumber.from('1000000000000000000'),
-  //     expectQuoteAmount: BigNumber.from('1500000000000000000'),
-  //     validFrom: 0,
-  //     validUntil: 9007199254740991,
-  //     maker: maker.tx,
-  //     taker: taker.tx,
-  //   } as any)
-  //   const { tx } = signedTransaction as any
-  //   expect(tx.signature.signature).to.eq(
-  //     '55ef8ca1910f545a4998ede5d6975aae71b2a8f631b7066febf832975fe041ae8fdeeab1ab2a4b23b36c3fdb7a2d7420c7c5dbe5a52c4e595524731b2dd6de01'
-  //   )
-  // })
-  it('sign sync order matching 2', async function () {
+describe('matching', () => {
+  it('bytes and signature', async function () {
     const wallet = await getWalletFromPrivateKey()
 
     const data = {
@@ -103,12 +25,13 @@ describe('Order', () => {
         amount: '100000000000000000000',
         isSell: 0,
         slotId: 1,
-        feeRatio: 10,
+        feeRatio1: 5,
+        feeRatio2: 10,
         accountId: 6,
         signature: {
           pubKey: '0dd4f603531bd78bbecd005d9e7cc62a794dcfadceffe03e269fbb6b72e9c724',
           signature:
-            '36525971db902fb7f8af094b24a835c9868b07599514670a93328da09357eb2e4dd3af60c89d401a682a9ec917ae8c4030a75e3dfc6f04aa9cecd17d13bb9101',
+            '4eb9eda2a7f3314dc8260d379c6d3f46429e554b9eeddd36ad9529d502bac5a4eedaf70eb1f76c0afd989cf4a4fb0da80ab9e07de932712e9dba27348861ae00',
         },
         validFrom: 0,
         validUntil: 9007199254740991,
@@ -123,12 +46,13 @@ describe('Order', () => {
         amount: '1000000000000000000',
         isSell: 1,
         slotId: 3,
-        feeRatio: 10,
+        feeRatio1: 5,
+        feeRatio2: 10,
         accountId: 6,
         signature: {
           pubKey: '0dd4f603531bd78bbecd005d9e7cc62a794dcfadceffe03e269fbb6b72e9c724',
           signature:
-            '44ebf850ea8a3324f8ddc42e0ba9925678cf79e9909d3b9281724dc7b3076028d40aef64bfe6fc78b2d427f4b3ea8867de1f7774d40bccdcb4cce946472d0c03',
+            'e76678aa2ab366f57bd5d1cceea0d7326b5b24a539494544c8dde75b2969882f976f8c2ea15c613d8b117e54bbc52375c2ffc547bb8999931f84c6ee7f572904',
         },
         validFrom: 0,
         validUntil: 9007199254740991,
@@ -152,8 +76,8 @@ describe('Order', () => {
     expect(Array.from(serializedMaker)).to.eql(
       [
         255, 0, 0, 0, 6, 1, 1, 0, 0, 0, 1, 0, 6, 0, 7, 0, 0, 0, 0, 0, 0, 0, 20, 209, 18, 13, 123,
-        22, 0, 0, 0, 10, 74, 129, 124, 128, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31, 255, 255, 255, 255,
-        255, 255,
+        22, 0, 0, 0, 5, 10, 74, 129, 124, 128, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31, 255, 255, 255,
+        255, 255, 255,
       ],
       'serialized maker is incorrect'
     )
@@ -161,30 +85,29 @@ describe('Order', () => {
     expect(Array.from(serializedTaker)).to.eql(
       [
         255, 0, 0, 0, 6, 1, 3, 0, 0, 0, 0, 0, 6, 0, 7, 0, 0, 0, 0, 0, 0, 0, 20, 209, 18, 13, 123,
-        22, 0, 0, 1, 10, 74, 129, 124, 128, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31, 255, 255, 255, 255,
+        22, 0, 0, 1, 5, 10, 74, 129, 124, 128, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31, 255, 255, 255, 255,
         255, 255,
       ],
       'serialized taker is incorrect'
     )
     const maker = await wallet.signSyncOrder(data.maker as any)
     const taker = await wallet.signSyncOrder(data.taker as any)
-
-    expect(maker.tx.signature).to.eql(data.maker.signature)
-    expect(taker.tx.signature).to.eql(data.taker.signature)
+    expect(maker.tx.signature).to.eql(data.maker.signature, 'maker signature is incorrect')
+    expect(taker.tx.signature).to.eql(data.taker.signature, 'taker signature is incorrect')
 
     const serialized = await serializeOrderMatching(data as any)
     expect(Array.from(serialized)).to.eql(
       [
         11, 0, 0, 0, 6, 52, 152, 244, 86, 100, 82, 112, 238, 0, 52, 65, 223, 130, 199, 24, 181, 108,
-        14, 102, 102, 241, 104, 100, 48, 30, 162, 254, 184, 214, 120, 15, 29, 8, 222, 135, 121, 7,
-        206, 184, 73, 146, 189, 124, 6, 231, 240, 64, 224, 104, 251, 221, 0, 7, 0, 0, 0, 0, 0, 0, 0,
+        14, 102, 102, 76, 85, 188, 226, 194, 164, 116, 92, 118, 12, 23, 201, 231, 44, 240, 176, 255,
+        224, 160, 185, 218, 203, 223, 73, 86, 220, 82, 92, 4, 128, 147, 0, 7, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 13, 224, 182, 179, 167, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 209, 18, 13, 123,
         22, 0, 0,
       ],
       'serialized order matching is incorrect'
     )
     expect(sha256(serialized)).to.eq(
-      '0x9606e83984e7174072331f4eec61385928886071bdfe197667114447172977fa',
+      '0x7481574dbbe3de82866c43dfac8889fc07796c7136309f91fa4ede5df34d8170',
       'sha256 hash is incorrect'
     )
 
@@ -192,7 +115,7 @@ describe('Order', () => {
     const { tx } = signedTransaction as any
     // wrong l2 signature: 46ab809c4a5beacfd423bb05332e818fb65d4d570939852206565c9a22adad8a3c3b4e69832c93f5b54b317bebcb44292bce5142f4bfa39f1b51a1cadcacae03
     expect(tx.signature.signature).to.eq(
-      'ed1a16e7682df3c5ce1befa6d9c0756d9f7f9bb758529836f43b0eb7e97c8b9bb1b2a98791d0482189128282eae78e61353e555a93b07f4d4b04397432b44605',
+      '6dbb217bb70055b4530608b331723d0ce00774962c8dd3a837ca634b33d405828ab38de23ccce1633834d8513b2e2f412f83df7e781f6b5456c5a5072dc6e503',
       'order matching signature is incorrect'
     )
   })
--- tests/order.test.ts
+++ tests/order.test.ts
@@ -23,7 +23,8 @@ const orderMaker = {
   price: BigNumber.from('10000000000000000000'),
   amount: BigNumber.from('5000000000000000000'),
   isSell: 0,
-  feeRatio: 100,
+  feeRatio1: 5,
+  feeRatio2: 10,
   validFrom: defaultValidFrom,
   validUntil: defaultValidUntil,
 }
@@ -38,7 +39,8 @@ const orderTaker = {
   price: BigNumber.from('5000000000000000000'),
   amount: BigNumber.from('10000000000000000000'),
   isSell: 1,
-  feeRatio: 100,
+  feeRatio1: 5,
+  feeRatio2: 10,
   validFrom: defaultValidFrom,
   validUntil: defaultValidUntil,
 }
@@ -47,14 +49,16 @@ describe('Order', () => {
   it('serialize order maker', () => {
     const serialized = serializeOrder(orderMaker as any)
     expect(Buffer.from(serialized).toString('hex')).eq(
-      'ff0000000201000000000000010002000000000000008ac7230489e8000000642540be40090000000000000000001fffffffffffff'
+      'ff0000000201000000000000010002000000000000008ac7230489e8000000050a2540be40090000000000000000001fffffffffffff',
+      'maker hex is incorrect'
     )
   })
 
   it('serialize order taker', () => {
     const serialized = serializeOrder(orderTaker as any)
     expect(Buffer.from(serialized).toString('hex')).eq(
-      'ff0000000201010000000000010002000000000000004563918244f4000001644a817c80090000000000000000001fffffffffffff'
+      'ff0000000201010000000000010002000000000000004563918244f4000001050a4a817c80090000000000000000001fffffffffffff',
+      'taker hex is incorrect'
     )
   })
 
@@ -75,84 +79,44 @@ describe('Order', () => {
     })
 
     expect(Buffer.from(serialized).toString('hex')).eq(
-      '0b00000002d5bda4d1f6b875b0f96397cf995f4251b77a910402a34fb9efd2c2caa522d1c750913818d830a9031bf74fb6659c14250cc35e00010000000000000000000006f05b59d3b2000000000000000000004563918244f40000'
+      '0b00000002d5bda4d1f6b875b0f96397cf995f4251b77a9104d9bcc0e6e1eaa3ac76c852d7506e4e569586cd43f6cd5f51dce7207467b53400010000000000000000000006f05b59d3b2000000000000000000004563918244f40000',
+      'order matching hex is incorrect'
     )
   })
 
   it('sign sync order', async function () {
     const wallet = await getWalletFromPrivateKey()
-    const signedTransaction = await wallet.signSyncOrder({
-      accountId: 13,
-      subAccountId: 1,
-      slotId: 0,
-      nonce: 0,
-      baseTokenId: 3,
-      quoteTokenId: 4,
-      isSell: 0,
-      feeRatio: 100,
-      amount: closestPackableTransactionAmount(parseEther('2')),
-      price: parseEther('1'),
-    } as any)
-    // expect(signedTransaction.ethereumSignature.signature).eq('0x1efd9e668351d47d5b20c446a2cdd0840dfee67e3a32fd1a7f7d7eb71784fa045b2e482c8fa9462c4df6351caa9a874fd2180301754cf7954c001c78341b28171c')
-    // expect(signedTransaction.tx.signature.pubKey).eq(
-    //   '0dd4f603531bd78bbecd005d9e7cc62a794dcfadceffe03e269fbb6b72e9c724'
-    // )
+    const signedTransaction = await wallet.signSyncOrder(orderMaker as any)
+    expect(signedTransaction.ethereumSignature.signature).eq(
+      '0x915d197bad1a7d6c3e840fd62607df97b007f9faa3843639342b62348170e3d700d0cc333d00630de65830d3028abcc81f4f2dc9caf71bcea5a31cc42dd9c2db1c'
+    )
+    expect(signedTransaction.tx.signature.pubKey).eq(
+      '0dd4f603531bd78bbecd005d9e7cc62a794dcfadceffe03e269fbb6b72e9c724'
+    )
     expect(signedTransaction.tx.signature.signature).eq(
-      'bef75b5d859baa9fbd421afa2ef56fd00eb4edfbd619647a8bcc2bf9df469a0789af97273bddf0ecf51ef1b2e8856e751a99b4ed614eda2067b1832684b23502'
+      '0e1b8df4a234422c3167725b98f591ae7b6ed5563f2d68786c82c966b1ad35a393a3d4a3570a9ebdb5d2dbdc9fc37c66d4d5ce682a2dd236e4073a5f078b9004'
     )
   })
 
-  it('sign sync order matching', async function () {
+  it('custom order', async function () {
     const wallet = await getWalletFromPrivateKey()
-    const maker = await wallet.signSyncOrder(orderMaker as any)
-    const taker = await wallet.signSyncOrder(orderTaker as any)
-    const bytes = await serializeOrderMatching({
-      type: 'OrderMatching',
-      accountId: 1,
-      account: '0x3498F456645270eE003441df82C718b56c0e6666',
-      fee: '0',
-      feeToken: 1,
+    const signedTransaction = await wallet.signSyncOrder({
+      subAccountId: 1,
+      accountId: 5,
+      slotId: 6,
       nonce: 0,
-      expectBaseAmount: '50000000000000',
-      expectQuoteAmount: '10000000000000',
-      validFrom: 0,
-      validUntil: 9007199254740991,
-      maker: maker.tx,
-      taker: taker.tx,
+      baseTokenId: 6,
+      quoteTokenId: 7,
+      price: BigNumber.from('900000000000000000'),
+      amount: BigNumber.from('10000000000000000000'),
+      isSell: 0,
+      feeRatio1: 5,
+      feeRatio2: 10,
+      validFrom: defaultValidFrom,
+      validUntil: defaultValidUntil,
     } as any)
-    expect(Buffer.from(bytes).toString('hex')).to.eq(
-      '0b000000013498f456645270ee003441df82c718b56c0e666602a34fb9efd2c2caa522d1c750913818d830a9031bf74fb6659c14250cc35e00010000000000000000000000002d79883d20000000000000000000000009184e72a000'
+    expect(signedTransaction.tx.signature.signature).eq(
+      'e84a62b9dc183b361ef2d81a1d02d91bf88c674fcbf9098772b3da01c3df299d791da8face13cb123ce403c777b45643881f045b3ea0f850a541cd819b93b300'
     )
-    const signedTransaction = await wallet.signSyncOrderMatching({
-      type: 'OrderMatching',
-      accountId: 1,
-      account: '0x3498F456645270eE003441df82C718b56c0e6666',
-      fee: BigNumber.from('0'),
-      feeToken: 1,
-      nonce: 0,
-      expectBaseAmount: BigNumber.from('50000000000000'),
-      expectQuoteAmount: BigNumber.from('10000000000000'),
-      validFrom: 0,
-      validUntil: 9007199254740991,
-      maker: maker.tx,
-      taker: taker.tx,
-    } as any)
-    const { tx } = signedTransaction as any
-    expect(tx.expectBaseAmount).to.eq('50000000000000')
-    expect(tx.expectQuoteAmount).to.eq('10000000000000')
-    expect(tx.fee).to.eq('0')
-
-    expect(tx.maker.accountId).to.eq(orderMaker.accountId)
-    expect(tx.maker.feeRatio).to.eq(orderMaker.feeRatio)
-    expect(tx.maker.price).to.eq(orderMaker.price.toString())
-    expect(tx.maker.amount).to.eq(orderMaker.amount.toString())
-
-    expect(tx.taker.accountId).to.eq(orderTaker.accountId)
-    expect(tx.taker.feeRatio).to.eq(orderTaker.feeRatio)
-    expect(tx.taker.price).to.eq(orderTaker.price.toString())
-    expect(tx.taker.amount).to.eq(orderTaker.amount.toString())
-
-    expect(signedTransaction.ethereumSignature.signature).to.not.empty
-    expect(tx.signature.signature).to.not.empty
   })
 })
