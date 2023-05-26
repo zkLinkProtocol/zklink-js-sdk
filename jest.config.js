2c811df80cc7bd6a60279e38007aed45246a725b
tyral <tyral@zklink.org>
2023-05-26 21:50:09 +0800

    chore: jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   watchAll: false,
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: [
     "__tests__/online/"
   ]
