065b4f14e8d72e2a6598d58b98954dacaff95127
tyral <tyral@zklink.org>
2023-06-06 14:44:50 +0800

    refactor: remove outdated functions

--- src/contract.ts
+++ src/contract.ts
@@ -1,19 +1,16 @@
+import { ErrorCode } from '@ethersproject/logger'
 import { BigNumber, BigNumberish, Contract, ContractTransaction, ethers } from 'ethers'
 import { Provider } from './provider'
-import { Address, TokenAddress } from './types'
+import { Address } from './types'
 import {
   ERC20_APPROVE_TRESHOLD,
-  ERC20_RECOMMENDED_FASTSWAP_GAS_LIMIT,
-  ETH_RECOMMENDED_FASTSWAP_GAS_LIMIT,
-  getFastSwapUNonce,
   IERC20_INTERFACE,
-  isTokenETH,
-  MAX_ERC20_APPROVE_AMOUNT,
   MAIN_CONTRACT_INTERFACE,
+  MAX_ERC20_APPROVE_AMOUNT,
   ZKL_CONTRACT_INTERFACE,
+  isTokenETH,
 } from './utils'
 import { ETHOperation } from './wallet'
-import { ErrorCode } from '@ethersproject/logger'
 
 const EthersErrorCode = ErrorCode
 
@@ -80,153 +77,6 @@ export class LinkContract {
     } catch (e) {
       this.modifyEthersError(e)
     }
-  }
-
-  async bridge(bridge: {
-    to: Address
-    toChainId: number
-    amount: BigNumberish
-    contractAddress: Address
-    ethTxOptions?: ethers.providers.TransactionRequest
-  }): Promise<ETHOperation> {
-    const zklContract = this.getZKLContract(bridge.contractAddress)
-
-    let ethTransaction
-
-    let uNonce: number = getFastSwapUNonce()
-
-    const lzFees = await zklContract.estimateBridgeFees(bridge.toChainId, bridge.to, bridge.amount)
-
-    const args = [
-      bridge.toChainId,
-      bridge.to,
-      bridge.amount,
-      {
-        value: lzFees,
-        ...bridge.ethTxOptions,
-      } as ethers.providers.TransactionRequest,
-    ]
-
-    // We set gas limit only if user does not set it using ethTxOptions.
-    const txRequest = args[args.length - 1] as ethers.providers.TransactionRequest
-    if (txRequest.gasLimit == null) {
-      try {
-        const gasEstimate = await zklContract.estimateGas.bridge(...args).then(
-          (estimate) => estimate,
-          () => BigNumber.from('0')
-        )
-        let recommendedGasLimit = ERC20_RECOMMENDED_FASTSWAP_GAS_LIMIT
-        txRequest.gasLimit = gasEstimate.gte(recommendedGasLimit)
-          ? gasEstimate
-          : recommendedGasLimit
-        args[args.length - 1] = txRequest
-      } catch (e) {
-        this.modifyEthersError(e)
-      }
-    }
-    console.log(args)
-    try {
-      ethTransaction = await zklContract.bridge(...args)
-    } catch (e) {
-      this.modifyEthersError(e)
-    }
-
-    return new ETHOperation(ethTransaction, this.provider)
-  }
-
-  async fastSwap(swap: {
-    fromChainId: number
-    toChainId: number
-    from: Address
-    to: Address
-    tokenInAddress: TokenAddress
-    tokenOutId: number
-    amountIn: BigNumberish
-    amountOutMin: BigNumberish
-    pair: Address
-    acceptTokenId: number
-    acceptAmountOutMin: BigNumberish
-    ethTxOptions?: ethers.providers.TransactionRequest
-    approveDepositAmountForERC20?: boolean
-  }): Promise<ETHOperation> {
-    const mainContract = await this.getMainContract(swap.fromChainId)
-
-    let ethTransaction
-    let uNonce: number = getFastSwapUNonce()
-
-    if (!uNonce) {
-      this.modifyEthersError(new Error('swap tx nonce is none'))
-    }
-    // -------------------------------
-    if (isTokenETH(swap.tokenInAddress)) {
-      try {
-        // function swapExactETHForTokens(address _zkSyncAddress,uint104 _amountOutMin, uint16 _withdrawFee, uint8 _toChainId, uint16 _toTokenId, address _to, uint32 _nonce) external payable
-        ethTransaction = await mainContract.swapExactETHForTokens(
-          swap.from,
-          swap.amountOutMin,
-          swap.toChainId,
-          swap.tokenOutId,
-          swap.to,
-          uNonce,
-          swap.pair,
-          swap.acceptTokenId,
-          swap.acceptAmountOutMin,
-          {
-            value: BigNumber.from(swap.amountIn),
-            gasLimit: BigNumber.from(ETH_RECOMMENDED_FASTSWAP_GAS_LIMIT),
-            ...swap.ethTxOptions,
-          }
-        )
-      } catch (e) {
-        this.modifyEthersError(e)
-      }
-    } else {
-      // ERC20 token deposit
-      let nonce: number
-      // function swapExactTokensForTokens(address _zkSyncAddress, uint104 _amountIn, uint104 _amountOutMin, uint16 _withdrawFee, IERC20 _fromToken, uint8 _toChainId, uint16 _toTokenId, address _to, uint32 _nonce) external
-      const args = [
-        swap.from,
-        swap.amountIn,
-        swap.amountOutMin,
-        swap.tokenInAddress,
-        swap.toChainId,
-        swap.tokenOutId,
-        swap.to,
-        uNonce,
-        swap.pair,
-        swap.acceptTokenId,
-        swap.acceptAmountOutMin,
-        {
-          nonce,
-          ...swap.ethTxOptions,
-        } as ethers.providers.TransactionRequest,
-      ]
-
-      // We set gas limit only if user does not set it using ethTxOptions.
-      const txRequest = args[args.length - 1] as ethers.providers.TransactionRequest
-      if (txRequest.gasLimit == null) {
-        try {
-          const gasEstimate = await mainContract.estimateGas.swapExactTokensForTokens(...args).then(
-            (estimate) => estimate,
-            () => BigNumber.from('0')
-          )
-          let recommendedGasLimit = ERC20_RECOMMENDED_FASTSWAP_GAS_LIMIT
-          txRequest.gasLimit = gasEstimate.gte(recommendedGasLimit)
-            ? gasEstimate
-            : recommendedGasLimit
-          args[args.length - 1] = txRequest
-        } catch (e) {
-          this.modifyEthersError(e)
-        }
-      }
-      try {
-        ethTransaction = await mainContract.swapExactTokensForTokens(...args)
-      } catch (e) {
-        this.modifyEthersError(e)
-      }
-    }
-
-    return new ETHOperation(ethTransaction, this.provider)
   }
 
   async getPendingBalance(pending: {
