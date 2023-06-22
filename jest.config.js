7ce57345733331ef23081cb5287902f028fd00aa
tyral <tyral@zklink.org>
2023-06-22 18:22:51 +0800

    chore: use swc jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
   watchAll: false,
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-  testPathIgnorePatterns: [
-    "__tests__/online/"
-  ]
-};
+  transform: {
+    '^.+\\.(t|j)s?$': [
+      '@swc-node/jest',
+    ],
+  },
+}
